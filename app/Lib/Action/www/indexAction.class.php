64df41456182637ed6d3597f8efdfaf73fa3ae08
ysc8620 <ysc8620@163.com>
2015-09-02 10:00:29 +0800

    ds

--- app/Lib/Action/www/indexAction.class.php
+++ app/Lib/Action/www/indexAction.class.php
@@ -9,6 +9,27 @@ class indexAction extends frontendAction {
 
     public function qrcode(){
         header("Content-type: text/html; charset=utf-8");
-        $this->display();
+        $type = $this->_param('type');
+        $page = intval($this->_param('p'));
+
+        $page = $page<1?1:$page;
+        $pagesize = 12;
+
+
+        $call_number = M('call_number');
+        $count = $call_number->where(array('state'=>0))->count();
+
+        $pager = new Page($count, $pagesize);
+        $call_number->limit($pager->firstRow.','.$pager->listRows);
+        $list = $call_number->select();
+        // print_r($list);
+        $this->assign('list', $list);
+        $this->assign('page', $page);
+        if($type){
+            $this->display('info');
+        }else{
+            $this->display();
+        }
+
     }
 }
