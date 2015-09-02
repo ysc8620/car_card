fd53ef298744983e8acd9a19ea53c5b0f3bffabe
ysc8620 <ysc8620@163.com>
2015-09-02 11:25:54 +0800

    ds

--- app/Lib/Action/www/cardAction.class.php
+++ app/Lib/Action/www/cardAction.class.php
@@ -2,8 +2,35 @@
 class cardAction extends frontendAction {
     
     public function index() {
+        $id = intval( $this->_param('id') );
+        $r = intval( $this->_param('r') );
+        if($id){
+            $call_number = M('call_number')->find($id);
+            $this->assign('call_number', $call_number);
+            if($call_number['rand_card_num'] != $r){
+                return $this->redirect('/');
+            }
 
-        echo 'card';
-        $this->display();
+            if(empty($call_number['info_id'])){
+                $this->assign('state', 0);
+                // 提示扫描B面完成用户绑定
+                $this->display('prompt');
+            }else{
+                $info = M('user_info')->find($call_number['info_id']);
+                if($info['state'] != 1){
+                    $this->assign('state', 1);
+                    // 锁定用户提示
+                    $this->display('prompt');
+                }else{
+                    // 显示
+                    $this->display();
+                }
+
+            }
+        }else{
+            return $this->redirect('/');
+        }
+
+
     }
 }
