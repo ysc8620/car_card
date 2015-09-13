fbf014287bfc59e347f15c1e09d2497154e00ec3
ysc8620 <ysc8620@163.com>
2015-09-14 07:12:26 +0800

    ds

--- SendTemplateSMS.php
+++ SendTemplateSMS.php
@@ -15,25 +15,31 @@
 include_once("./CCPRestSmsSDK.php");
 
 //主帐号,对应开官网发者主账号下的 ACCOUNT SID
+global $accountSid;
 $accountSid= 'aaf98f894f402f15014f45458d53047a';
 
 //主帐号令牌,对应官网开发者主账号下的 AUTH TOKEN
+global $accountToken;
 $accountToken= '08f5b4411878401eb90b59f70eb92f7c';
 
 //应用Id，在官网应用列表中点击应用，对应应用详情中的APP ID
 //在开发调试的时候，可以使用官网自动为您分配的测试Demo的APP ID
+global $appId;
 $appId='aaf98f894f4900a3014f4b529bb20304';
 
 //请求地址
 //沙盒环境（用于应用开发调试）：sandboxapp.cloopen.com
 //生产环境（用户应用上线使用）：app.cloopen.com
+global $serverIP;
 $serverIP='sandboxapp.cloopen.com';
-echo $serverIP;
+
 
 //请求端口，生产环境和沙盒环境一致
+global $serverPort;
 $serverPort='8883';
 
 //REST版本号，在官网文档REST介绍中获得。
+global $softVersion;
 $softVersion='2013-12-26';
 
 
